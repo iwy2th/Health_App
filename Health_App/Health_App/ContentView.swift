72bdd4128fd9382e815a1fb64cdc362ffb0b19f0
iwy2th <huyloind03@gmail.com>
2023-05-14 17:22:17 +0700

    Test

---
+++ Health_App/Health_App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Health_App
+//
+//  Created by Iwy2th on 14/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Health_App/Health_App/Health_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_AppApp.swift
+//  Health_App
+//
+//  Created by Iwy2th on 14/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Health_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

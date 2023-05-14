c834426e113a17c1f07883f75b9d281298a83719
iwy2th <huyloind03@gmail.com>
2023-05-14 23:33:08 +0700

    Change Exercise View

--- Health_App/Health_App/Views/ExerciseView.swift
+++ Health_App/Health_App/Views/ExerciseView.swift
@@ -5,13 +5,31 @@ struct ExerciseView: View {
   let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
   let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
   let index: Int
-    var body: some View {
+  var body: some View {
+    VStack {
+      VStack {
         Text(exerciseNames[index])
+          .font(.largeTitle)
+        HStack {
+          Image(systemName: "1.circle")
+          Image(systemName: "2.circle")
+          Image(systemName: "3.circle")
+          Image(systemName: "4.circle")
+        }
+        .font(.title2)
+      }
+      Text("Video Player")
+      Text("Timer")
+      Text("Start/Done Button")
+      Text("Rating")
+      Text("History button")
     }
+
+  }
 }
 
 struct ExerciseView_Previews: PreviewProvider {
-    static var previews: some View {
-      ExerciseView(index: 0)
-    }
+  static var previews: some View {
+    ExerciseView(index: 0)
+  }
 }

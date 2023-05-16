134bce3becd4499ce48ed785fac4c5c38ba849c9
iwy2th <huyloind03@gmail.com>
2023-05-16 14:25:38 +0700

    ban dia hoa

---
+++ Health_App/Health_App/Views/SuccessView.swift
@@ -0,0 +1,43 @@
+//
+//  SuccessView.swift
+//  Health_App
+//
+//  Created by Iwy2th on 15/05/2023.
+//
+
+import SwiftUI
+
+struct SuccessView: View {
+  var body: some View {
+    ZStack {
+      VStack {
+        Image(systemName: "hand.raised.fill")
+          .resizedToFill(width: 75, height: 75)
+          .foregroundColor(.purple)
+        Text("High Five!")
+          .font(.largeTitle)
+          .fontWeight(.heavy)
+        Text("""
+        Good job completing all four exercises!
+        Remember tomorrow's another day.
+        So eat well and get some rest.
+        """)
+        .multilineTextAlignment(.center)
+        .foregroundColor(.gray)
+      }
+      VStack {
+        Spacer()
+        Button("Continue") {
+
+        }
+        .padding()
+      }
+    }
+  }
+}
+
+struct SuccessView_Previews: PreviewProvider {
+  static var previews: some View {
+    SuccessView()
+  }
+}
